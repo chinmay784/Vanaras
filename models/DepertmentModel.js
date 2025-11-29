e7ad457de509dcee292e0ff1aff325262f7e8301
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-11-29 11:46:22 +0530

    edited

--- models/DepertmentModel.js
+++ models/DepertmentModel.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const DepertmentSchema = new mongoose.Schema({
     DepartmentName: { type: String, trim: true },
     people: [{
-        type: String, trim: true,
-        default: null,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Employee",
     }]
 });
 
-module.exports = mongoose.model('Depertment',DepertmentSchema);
+module.exports = mongoose.model('Depertment', DepertmentSchema);
