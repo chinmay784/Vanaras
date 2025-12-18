82fd8e9ce4e0743a6148f695bd0efa8da5ab2d2c
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-12-18 15:49:45 +0530

    edited

--- database/Db.js
+++ database/Db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.DBURL);
+        await mongoose.connect("mongodb+srv://admin_db_user:IMVZFc9NDiGRyWGj@cluster0.kaohhih.mongodb.net/ProductionGPSDevices");
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection failed:', error);
